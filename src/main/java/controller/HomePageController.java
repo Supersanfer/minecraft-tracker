4ac052f15f689e34f4530d050c0e7025b75c593f
AlvG <Supersanfer>
2025-05-06 20:46:17 +0200

    Beginning of integration of created worlds as buttons on the main page.

--- src/main/java/controller/HomePageController.java
+++ src/main/java/controller/HomePageController.java
@@ -5,7 +5,12 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
+import model.MinecraftWorld;
+import repository.MinecraftWorldRepository;
 import services.StageService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomePageController {
 
@@ -26,5 +31,32 @@ public class HomePageController {
 		//	Creamos nueva pestaña formulario pidiendo todos los datos.
 		StageService.createStage("Create new World", "createWorld-view.fxml");
 	}
+
+	@FXML
+	void initialize() {
+		List<MinecraftWorld> worldList;
+		worldList = new MinecraftWorldRepository().getAll();
+		if(worldList.isEmpty()) {
+			btnCreateWorld.setLayoutX(200);
+			btnCreateWorld.setLayoutY(200);
+			btnCreateWorld.prefHeight(400);
+			btnCreateWorld.prefWidth(400);
+		}
+		else {
+			worldList.forEach(world -> createButtonEachWorld(world));
+		}
+	}
+
+	private void createButtonEachWorld(MinecraftWorld world) {
+		Button btnCreatedWorld = new Button(world.getName());
+		btnCreatedWorld.setPrefHeight(100);
+		btnCreatedWorld.setPrefWidth(200);
+		btnCreatedWorld.setOnMouseClicked(e->showWorldInfo());
+	}
+
+	private void showWorldInfo() {
+
+	}
+
 
 }
